00e66d05e4565b29bab74f2d984b50eaa7c3b677
Tony <anthoska@hotmail.es>
2021-04-22 12:27:10 +0200

    segunda clase

---
+++ actividad4/src/actividad4/Actividad4_2.java
@@ -0,0 +1,10 @@
+package actividad4;
+
+public class Actividad4_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Segunda clase");
+	}
+
+}
